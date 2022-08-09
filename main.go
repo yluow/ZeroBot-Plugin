ff248f1434bd3b8e28c39680130af4f38f2854c2
yluow <50765827+yluow@users.noreply.github.com>
2022-08-09 09:22:06 +0800

    test1

--- main.go
+++ main.go
@@ -112,7 +112,7 @@ import (
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/tiangou"       // 舔狗日记
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/tracemoe"    // 搜番
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/translation" // 翻译
-	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/vtb_quotation" // vtb语录
+	 _ "github.com/FloatTech/ZeroBot-Plugin/plugin/vtb_quotation" // vtb语录
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wangyiyun" // 网易云音乐热评
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/word_count"    // 聊天热词
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordle"        // 猜单词
